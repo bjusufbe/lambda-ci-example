dcfbc179778d10b235e8ff792c7bb911614a0de6
bjusufbe <bakir@atlantbh.com>
2020-09-28 15:01:06 +0200

    Added dep. for test purpose

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { UserData } from "./lib/userData";
 
 export const handler = async (event: any = {}): Promise<any> => {
     let userData = new UserData(event.firstName, event.lastName, event.age);
+    userData.outputDataInYamlFormat();
     const response = JSON.stringify(`Hello from Lambda! User data: ${userData.getUserDataInfo()}`);
     return response;
 }
--- lib/userData.ts
+++ lib/userData.ts
@@ -1,3 +1,6 @@
+// convert data to yaml just for the purpose of using dependency
+const yaml = require('js-yaml');
+
 export class UserData {
     protected firstName: string;
     protected lastName: string;
@@ -11,5 +14,14 @@ export class UserData {
 
     public getUserDataInfo() {
         return `First name: ${this.firstName}, Last name: ${this.lastName}, Age: ${this.age}`
+    }
+
+    public outputDataInYamlFormat() {
+        let data = {
+            firstName: this.firstName,
+            lastName: this.lastName,
+            age: this.age
+        }
+        console.dir(yaml.safeDump(data), { colors: true });
     }
 }
